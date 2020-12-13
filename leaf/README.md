22ee010f85e2b3307aebe6d0b115c084e9dcd4df
Zhenyang Hua <edison.hua@gmail.com>
2020-12-12 21:45:53 -0500

    Added demo

---
+++ leaf/README.md
@@ -0,0 +1,19 @@
+# houdini-leaf
+
+[Demo](https://codepen.io/zhua/pen/LYRxxJK)
+
+![Three boxes with leaf and vine in the background](houdini-leaf-demo.png)
+
+Import the script
+```html
+<script src="https://unpkg.com/houdini-leaf"></script>
+```
+
+Default settings
+```css
+.my-element {
+    --leaf-color: black;
+    --leaf-size: 16;
+    background-image: paint(leaf);
+}
+```
--- leaf/worklet.js
+++ leaf/worklet.js
@@ -1,42 +1,46 @@
-class LeafPainter {
-  static get inputProperties() { return ['--leaf-size', '--leaf-color']; }
+const randomInt = (max) => Math.floor(Math.random() * Math.floor(max));
 
-  paint(ctx, geom, properties) {
-    const leafSize = parseInt(properties.get('--leaf-size')) || 16;
-    const numLeaves = Math.floor(geom.height / leafSize) * 1.5;
-    const x = leafSize * 2 + 1;
-    
-    ctx.strokeStyle = properties.get('--leaf-color') || black;
-    ctx.fillStyle = properties.get('--leaf-color') || black;
-    this.vine(ctx, geom, x, numLeaves, leafSize);
-  }
+if (typeof registerPaint !== 'undefined') {
+  class LeafPainter {
+    static get inputProperties() { return ['--leaf-size', '--leaf-color']; }
 
-  leaf(ctx, x, y, size, dir) {
-    ctx.translate(x, y);
-    ctx.scale(size, size);
-    ctx.beginPath();
-    ctx.moveTo(dir, -0.7);
-    ctx.bezierCurveTo(dir, -0.7, 0.4 * dir, -1, 0, 0);
-    ctx.bezierCurveTo(0, 0, dir, 0.4, dir, -0.7);
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-    ctx.fill();
-  }
+    paint(ctx, geom, properties) {
+      const leafSize = parseInt(properties.get('--leaf-size')) || 16;
+      const numLeaves = Math.floor(geom.height / leafSize) * 1.5;
+      const x = leafSize * 2 + 1;
 
-  vine(ctx, geom, x, numLeaves, leafSize) {
-    const width = geom.width;
-    const height = geom.height;
-    ctx.beginPath();
-    ctx.moveTo(x, 0);
-    ctx.lineTo(x, height);
-    ctx.stroke();
-    const gap = height / numLeaves;
-    let direction = 1;
-    for (let i = 0; i < numLeaves; i++) {
-      const r = Math.floor(Math.random() * Math.floor(gap));
-      this.leaf(ctx, x, gap * i + r, leafSize, direction);
-      direction = -direction;
+      ctx.strokeStyle = properties.get('--leaf-color') || black;
+      ctx.fillStyle = properties.get('--leaf-color') || black;
+      this.vine(ctx, geom, x, numLeaves, leafSize);
+    }
+
+    leaf(ctx, x, y, size, dir) {
+      ctx.translate(x, y);
+      ctx.scale(size, size);
+      ctx.beginPath();
+      ctx.moveTo(dir, -0.7);
+      ctx.bezierCurveTo(dir, -0.7, 0.4 * dir, -1, 0, 0);
+      ctx.bezierCurveTo(0, 0, dir, 0.4, dir, -0.7);
+      ctx.setTransform(1, 0, 0, 1, 0, 0);
+      ctx.fill();
+    }
+
+    vine(ctx, geom, x, numLeaves, leafSize) {
+      const width = geom.width;
+      const height = geom.height;
+      ctx.beginPath();
+      ctx.moveTo(x, 0);
+      ctx.lineTo(x, height);
+      ctx.stroke();
+      const gap = height / numLeaves;
+      let direction = 1;
+      for (let i = 0; i < numLeaves; i++) {
+        const r = randomInt(gap);
+        this.leaf(ctx, x, gap * i + r, leafSize, direction);
+        direction = -direction;
+      }
     }
   }
-}
 
-registerPaint('leaf', LeafPainter);
+  registerPaint('leaf', LeafPainter);
+}
